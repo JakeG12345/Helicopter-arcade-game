8c1aa95b7d11c448aed638638d7a9fc476ad07b6
Jake Glick <jake@glickpeople.com>
2021-06-12 15:04:25 +1000

    Initial Commit

---
+++ Helicopter arcade game/Helicopter_arcade_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helicopter_arcade_gameApp.swift
+//  Helicopter arcade game
+//
+//  Created by Jake Glick on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Helicopter_arcade_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
